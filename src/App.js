05d3985b65644428a5e176ad5129d0ea265dc1b9
elfraim <elfraiman@gmail.com>
2019-04-04 23:10:32 +0200

    feat(grid): added css grid and custom hr lines

--- src/App.js
+++ src/App.js
@@ -6,13 +6,29 @@ import ElanHeader from './components/Header/Header';
 // design see @ https://dribbble.com/shots/3860051-Personal-Portfolio-Site
 
 class App extends Component {
+
+ColoredLine = ( color ) => (
+    <hr style={{
+      marginTop: 10,
+      marginLeft: 10,
+      color: color,
+      backgroundColor: color,
+      height: 2,
+      border: 'none'
+    }}  />
+  );
+
   render() {
     return (
       <div className="App">
         <ElanHeader />
 
-        <div className="middle">
-          Hello
+        <div className="wrapper">
+          <p className="hello-text">Hello</p>
+          <div className="white-line">
+            {this.ColoredLine('#aa00ff')}
+          </div>
+          <p className="this-is-me">This is me</p>
         </div>
       </div>
     );
